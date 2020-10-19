5ccf8d9e194d66773a4e71521d603c0d59a00046
Hanna Baum <0583220339hana@gmail.com>
2020-10-19 19:27:46 +0300

    myProject.sln

--- myProject/myProject/Program.cs
+++ myProject/myProject/Program.cs
@@ -7,6 +7,7 @@ namespace myProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hey I'm here");
         }
     }
 }
